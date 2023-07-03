6f0e39ca93ec3cbe9cb467291949972b38c89ade
korir desmond <korirdesmond@MacBook-Air.local>
2023-07-03 19:31:17 +0300

    Integrate login

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,11 @@ import { authSelector } from "./features/auth";
 import Home from './pages/Home'
 import ErrorPage from './components/atoms/ErrorPage';
 import Projects from './pages/Projects';
+<<<<<<< HEAD
 import { Login, Signup } from './pages/Auth';
+=======
+import { Login } from './pages/Auth';
+>>>>>>> 93399ba (Integrate login)
 import Profile from './pages/Profile';
 
 function App() {
@@ -51,10 +55,13 @@ function App() {
           element={<Login />}
         />
         <Route
+<<<<<<< HEAD
           path='signup'
           element={<Signup />}
         />
         <Route
+=======
+>>>>>>> 93399ba (Integrate login)
           path='profile'
           element={
             <PrivateRoute>
